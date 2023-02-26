3f63d13be47ae667f17b530a1fa2069d2925191f
sajjadalijaved <sajjadali11755@gmail.com>
2023-02-26 21:38:28 +0500

    save

--- lib/Bloc/BottomNavigationBarBloc/bottom_navigation_bar_state.dart
+++ lib/Bloc/BottomNavigationBarBloc/bottom_navigation_bar_state.dart
@@ -3,6 +3,7 @@ part of 'bottom_navigation_bar_bloc.dart';
 @immutable
 abstract class BottomNavigationBarState {}
 
+// ignore: must_be_immutable
 class BottomNavigationBarInitial extends BottomNavigationBarState {
   int currentIndex;
   BottomNavigationBarInitial({required this.currentIndex});
